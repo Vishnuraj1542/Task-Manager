daddb6bbb5f4bffe229e8f222c169d4078017f69
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-08-22 20:10:23 +0530

    views and urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
-    path("api/accounts/", include("accounts.urls")),
-    path("api/tasks/", include("task.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("tasks/", include("task.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
     
